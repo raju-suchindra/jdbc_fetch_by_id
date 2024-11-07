f8ee52aeb3913baaf4ff9b598b318a3bb4272720
Padmaraju Suchindra <126499020+raju-suchindra@users.noreply.github.com>
2024-11-07 22:56:44 +0530

    Update fetchSpecificData.java

--- fetchSpecificData.java
+++ fetchSpecificData.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Scanner;
-import java.util.*;
 import first_jdbc_pgm.MyCon;
 
 public class fetchSpecificData {
